681540d6a2b43ffd26542a495fd2d94d7c508b35
Babar Shafiq Nazmi <bshafiq@silicontechnix.com>
2013-03-19 14:42:12 +0500

    Update fullbackup.php
    Adding feature of Expiry of backup files in days

--- fullbackup.php
+++ fullbackup.php
@@ -12,22 +12,32 @@ $ftphost = "ip_or_hostname_of_ftp"; // FTP host IP or domain name
 $ftpacct = "userid"; // FTP account
 $ftppass = "password"; // FTP password
 $email_notify = 'your_email@domain.com'; // Email address for backup notification
- 
+$backupexpireindays=21; //3 weeks expire time in days, 21 days = 7*24*60
+$backupexpireindays=($backupexpireindays*24)*3600; //convert it to seconds, 24 hours * 60 minutes * 60 seconds
+
 $xmlapi = new xmlapi($source_server_ip);
 $xmlapi--->password_auth($cpanel_account,$cpanel_password);
 $xmlapi->set_port('2083');
  
-// Delete any other backup before create new backup
+// Delete any other backup with filetime greater than expire time, before create new backup
 $conn_id = ftp_connect($ftphost);
 $login_result = ftp_login($conn_id, $ftpacct, $ftppass);
 $logs_dir = "/";
- 
 ftp_chdir($conn_id, $logs_dir);
 $files = ftp_nlist($conn_id, ".");
-foreach ($files as $file){
-    ftp_delete($conn_id, $file);
+foreach ($files as $filename) {
+        $fileCreationTime = ftp_mdtm($conn_id, $filename);
+        $date = date("F j, Y, g:i a", ftp_mdtm($conn_id, $filename));
+        //print "<br>Timestamp of '$filename': $date";
+        $fileAge=time();
+        $fileAge=$fileAge-$fileCreationTime;
+        if ($fileAge > $backupexpireindays) { // Is the file older than the given time span?
+               //echo "<br>The file $filename is older than Expire time :$expiretime ...Deleting\n";
+               ftp_delete($conn_id, $filename);
+               //echo "<br>Deleted<br><br>";
+               }
 }
- 
+
 ftp_close($conn_id);
  
 $api_args = array(
