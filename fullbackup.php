543cf17257d1f9789a5840bd001b072f9503b935
Babar Shafiq Nazmi <bshafiq@silicontechnix.com>
2013-03-20 12:54:06 +0500

    Update fullbackup.php
    Comments added for Config section, so editing only required in that section
    Comment out DATE formatting for Displaying File Timestamp

--- fullbackup.php
+++ fullbackup.php
@@ -2,17 +2,23 @@
 // Must include cPanel API
 include "xmlapi.php";
  
+//CONFIG SECTION
+/*******************************************************/
 // Credentials for cPanel account
 $source_server_ip = "your_domain_or_IP"; // Server IP or domain name eg: 212.122.3.77 or cpanel.domain.tld
 $cpanel_account = "userid"; // cPanel username
 $cpanel_password = "password"; // cPanel password
- 
 // Credentials for FTP remote site
 $ftphost = "ip_or_hostname_of_ftp"; // FTP host IP or domain name
 $ftpacct = "userid"; // FTP account
 $ftppass = "password"; // FTP password
+$logs_dir = "/"; //FTP Remote Folder
 $email_notify = 'your_email@domain.com'; // Email address for backup notification
 $backupexpireindays=21; //3 weeks expire time in days, 21 days = 7*24*60
+//END OF CONFIG SECTION
+/*******************************************************/
+//Do not edit below this line
+
 $backupexpireindays=($backupexpireindays*24)*3600; //convert it to seconds, 24 hours * 60 minutes * 60 seconds
 
 $xmlapi = new xmlapi($source_server_ip);
@@ -22,12 +28,12 @@ $xmlapi->set_port('2083');
 // Delete any other backup with filetime greater than expire time, before create new backup
 $conn_id = ftp_connect($ftphost);
 $login_result = ftp_login($conn_id, $ftpacct, $ftppass);
-$logs_dir = "/";
+
 ftp_chdir($conn_id, $logs_dir);
 $files = ftp_nlist($conn_id, ".");
 foreach ($files as $filename) {
         $fileCreationTime = ftp_mdtm($conn_id, $filename);
-        $date = date("F j, Y, g:i a", ftp_mdtm($conn_id, $filename));
+        //$date = date("F j, Y, g:i a", ftp_mdtm($conn_id, $filename));
         //print "<br>Timestamp of '$filename': $date";
         $fileAge=time();
         $fileAge=$fileAge-$fileCreationTime;
